295112ae4694949c83cf34c1d9cd17b4a4756aac
Andres Avila <anavrojas@hotmail.com>
2020-09-16 19:15:50 -0500

    Obtencion Analoga
    Esta version permite obtener las señales análogas de voltaje y corriente en todo el tiempo del archivo

--- test1.py
+++ test1.py
@@ -8,10 +8,11 @@ Created on Wed Sep 16 10:06:16 2020
 import matplotlib.pyplot as plt
 import pyComtrade
 comtradeObj = pyComtrade.ComtradeRecord()
-# comtradeObj.read('./Comtrade TEST 1-1/L1L2L3_75%/BINAIRE/Test_1-1_ABC60_75%_BIN.cfg', './Comtrade TEST 1-1/L1L2L3_75%/BINAIRE/Test_1-1_ABC60_75%_BIN.dat')
-comtradeObj.read('./test_data3/test3.cfg', './test_data3/test3.dat')
+comtradeObj.read('./Comtrade TEST 1-1/L1L2L3_75%/BINAIRE/Test_1-1_ABC60_75%_BIN.cfg', './Comtrade TEST 1-1/L1L2L3_75%/BINAIRE/Test_1-1_ABC60_75%_BIN.dat')
+# comtradeObj.read('./test_data3/test3.cfg', './test_data3/test3.dat')
 
-print(comtradeObj.get_analog_ids())  # print the ids of the analog channels.
+print('Los analogos',comtradeObj.get_analog_ids())  # print the ids of the analog channels.
+print('Los digitales',comtradeObj.get_digital_ids())  # print the ids of the analog channels.
 
 N = comtradeObj['endsamp'][-1]
 
@@ -19,9 +20,9 @@ print('Record has {} samples'.format(N))
 print('Sampling rate is {} samples/sec.'.format(comtradeObj['samp'][-1]))
 
 # Reading channel 4:
-AnalogChannelData = comtradeObj['A'][22]['values']
+AnalogChannelData = comtradeObj['A'][1]['values']
 
-DigitalChannelData = comtradeObj['D'][25]['values']
+DigitalChannelData = comtradeObj['A'][4]['values']
 
 # Reading time vector:
 time = comtradeObj.get_timestamps()
@@ -34,7 +35,7 @@ axarr[0].plot(time, AnalogChannelData)
 axarr[0].set_title('pyComtrade Demo')
 axarr[0].grid()
 axarr[1].plot(time, DigitalChannelData)
-axarr[1].set_ylim(top=1.05)  # bottom unchanged
+axarr[1].set_ylim()  # bottom unchanged
 axarr[1].grid()
 axarr[1].set_xlabel('Time [s]')
 plt.show()
